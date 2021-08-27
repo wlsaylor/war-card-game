53e371a2df70b6477a433b1c215aa908954fdd45
William Saylor <wsaylor@gmail.com>
2021-08-26 18:55:23 -0600

    Added shuffle method

--- main.js
+++ main.js
@@ -14,12 +14,26 @@ class Deck {
         const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
         'Jack', 'Queen', 'King', 'Ace'];
         const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-    
+        
+        //Create each card in the deck by looping over the suits, ranks, and values
         for (let i = 0; i < suits.length; i++) {
             for (let j = 0; j < ranks.length; j++) {
                 this.deck.push(new Card(ranks[j], values[j], suits[i]));
             }
         }
+
+    }
+
+    // Shuffle in place using the Durstenfeld shuffle 
+    shuffle() {
+        for (let i = this.deck.length - 1; i > 0; i--) {
+            let randomCard = Math.floor(Math.random() * (i + 1));
+            let currentCard = this.deck[i];
+            this.deck[i] = this.deck[randomCard];
+            this.deck[randomCard] = currentCard;
+        }
+
+        return this.deck;
     }
 }
 
