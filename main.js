b360a0acf12a9813dea21c616f05f57f676882bf
William Saylor <wsaylor@gmail.com>
2021-08-26 21:12:39 -0600

    Added player class and deal method

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.hand = [];
+    }
+}
+
 class Card {
     constructor(rank, value, suit) {
         this.rank = rank;
@@ -13,27 +20,32 @@ class Deck {
         const suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
         const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
         'Jack', 'Queen', 'King', 'Ace'];
-        const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
         
-        //Create each card in the deck by looping over the suits, ranks, and values
+        //Create each card in the deck by looping over the suits and ranks. 
+        //Values assigned from rank index low to high
         for (let i = 0; i < suits.length; i++) {
             for (let j = 0; j < ranks.length; j++) {
-                this.deck.push(new Card(ranks[j], values[j], suits[i]));
+                this.deck.push(new Card(ranks[j], j + 2, suits[i]));
             }
         }
 
     }
 
-    // Shuffle in place using the Durstenfeld shuffle 
+    // Shuffle in place method using the Durstenfeld shuffle randomly swapping card indexes
     shuffle() {
         for (let i = this.deck.length - 1; i > 0; i--) {
-            let randomCard = Math.floor(Math.random() * (i + 1));
-            let currentCard = this.deck[i];
-            this.deck[i] = this.deck[randomCard];
-            this.deck[randomCard] = currentCard;
+            let randomIndex = Math.floor(Math.random() * (i + 1));
+            let currentIndex = this.deck[i];
+            this.deck[i] = this.deck[randomIndex];
+            this.deck[randomIndex] = currentIndex;
         }
 
         return this.deck;
+    }
+
+    deal(firstPlayer, secondPlayer) {
+        firstPlayer.hand = [...this.deck.slice(0,26)];
+        secondPlayer.hand = [...this.deck.slice(26, 52)];
     }
 }
 
